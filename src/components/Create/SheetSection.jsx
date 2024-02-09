f903fc2ffa1e9d3983664d3e96cebc16eea250b3
kimabraham <kimkr88@icloud.com>
2024-02-09 19:17:54 +0900

    Feat. apply loading modal and sheet section form tag change to div

--- src/components/Create/SheetSection.jsx
+++ src/components/Create/SheetSection.jsx
@@ -28,8 +28,8 @@ function SheetSection() {
     onSettled: () => {},
   });
 
-  const handleSheetUrlSubmit = e => {
-    e.preventDefault();
+  const handleOpen = () => {
+    console.log("open");
   };
 
   const handleGenerateSheetUrl = () => {
@@ -41,21 +41,20 @@ function SheetSection() {
       <h2 className="font-semi-bold text-xl text-text-950 mb-2">
         2. Google Spread sheet Definition
       </h2>
-      <form
-        onSubmit={handleSheetUrlSubmit}
-        className="sm:flex sm:items-baseline sm:gap-4 space-y-2 sm:[&>*:first-child]:w-8/12 sm:[&>*:nth-child(2)]:w-2/12 sm:[&>*:last-child]:w-2/12 mt-4"
-      >
+      <div className="sm:flex sm:items-baseline sm:gap-4 space-y-2 sm:[&>*:first-child]:w-8/12 sm:[&>*:nth-child(2)]:w-2/12 sm:[&>*:last-child]:w-2/12 mt-4">
         <FloatingInput
           type="text"
           name="sheetUrl"
           label="Google Spread Sheet Url"
           {...sheetUrl}
         />
-        <FormButton handleClick={handleGenerateSheetUrl} type="button">
+        <FormButton type="button" handleClick={handleGenerateSheetUrl}>
           Generate
         </FormButton>
-        <FormButton type="submit">Submit</FormButton>
-      </form>
+        <FormButton type="button" handleClick={handleOpen}>
+          Submit
+        </FormButton>
+      </div>
       {sheetUrl.error && (
         <FormError errorMessage={sheetUrl.error} setShow={sheetUrl.setError} />
       )}
--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,23 +1,17 @@
-import SyncButton from "../../components/Form/SyncButton";
 import MongoSection from "../../components/Create/MongoSection";
 import SheetSection from "../../components/Create/SheetSection";
+import Modal from "../../components/shared/Modal";
 
 function CreateProject() {
-  const handleSynchronize = e => {
-    e.preventDefault();
-  };
-
   return (
     <div className="flex justify-center items-center h-full">
-      <div className="flex flex-col space-y-4 xl:w-8/12 2xl:w-6/12 mx-auto max-w-screen-xl">
+      <div className="flex flex-col xl:w-8/12 2xl:w-6/12 mx-auto max-w-screen-xl">
         <h1 className="font-bold text-2xl text-text-950 uppercase mb-2">
           Create Project
         </h1>
         <MongoSection />
         <SheetSection />
-        <SyncButton type="button" handleClick={handleSynchronize}>
-          Synchronize
-        </SyncButton>
+        <Modal />
       </div>
     </div>
   );
