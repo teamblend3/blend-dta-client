130b4be58f1e863bc1a45418f711ee52039b5176
kimabraham <kimkr88@icloud.com>
2024-02-22 02:13:42 +0900

    Refactor:Dark theme manage in local storage

--- src/stores/useThemeStore.js
+++ src/stores/useThemeStore.js
@@ -1,8 +1,18 @@
 import { create } from "zustand";
 
+const getInitialTheme = () => {
+  const isDark = localStorage.getItem("isDark");
+  return isDark === "true";
+};
+
 const useThemeStore = create(set => ({
-  isDark: false,
-  toggleTheme: () => set(state => ({ isDark: !state.isDark })),
+  isDark: getInitialTheme(),
+  toggleTheme: () =>
+    set(state => {
+      const newIsDark = !state.isDark;
+      localStorage.setItem("isDark", newIsDark.toString());
+      return { isDark: newIsDark };
+    }),
 }));
 
 export default useThemeStore;
