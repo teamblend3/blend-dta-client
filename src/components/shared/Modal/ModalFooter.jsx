5ebea097a7490b532088d797d99a6f8778669e39
kimabraham <kimkr88@icloud.com>
2024-02-26 19:45:57 +0900

    Feat:when Mock auth, download excel file after sync

--- src/components/shared/Modal/ModalFooter.jsx
+++ src/components/shared/Modal/ModalFooter.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import { SiGooglesheets } from "react-icons/si";
 import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
 import Button from "../../Button/Button";
@@ -9,8 +8,15 @@ import {
   GO_TO_SHEET_BUTTON_STYLE,
   CLOSE_MODAL_BUTTON_STYLE,
 } from "../../../utils/styleConstants";
+import useAuthStore from "../../../stores/useAuthStore";
 
 function ModalFooter({ onCloseModal }) {
+  const {
+    userInfo: { userId },
+  } = useAuthStore();
+  const {
+    projectInfo: { sheetUrl },
+  } = useProjectStore();
   const stepStatus = useTaskStatus();
   const allStepsDone = stepStatus.every(step => step.done);
 
@@ -34,9 +40,14 @@ function ModalFooter({ onCloseModal }) {
 
       {allStepsDone && (
         <a
-          href={useProjectStore.getState().projectInfo.sheetUrl}
+          href={sheetUrl}
           target="_blank"
           rel="noreferrer"
+          download={
+            userId === import.meta.env.VITE_MOCK_AUTH_ID
+              ? "sample.xlsx"
+              : undefined
+          }
         >
           <Button type="button" style={GO_TO_SHEET_BUTTON_STYLE}>
             Check your Sheet
