15b3cceb94246eac05070bf07277328beb2ef3b0
kimabraham <kimkr88@icloud.com>
2024-02-27 16:51:52 +0900

    Refactor:Add Loading line Component when download excel

--- src/pages/Collection/Schema.jsx
+++ src/pages/Collection/Schema.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
-import { RiFileExcel2Line } from "react-icons/ri";
 import { useParams } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { RiFileExcel2Line } from "react-icons/ri";
+import LoadingLine from "../../components/shared/LoadingLine";
 
 function Schema({ schemas, collection }) {
   const [selectedRows, setSelectedRows] = useState([]);
@@ -33,47 +35,67 @@ function Schema({ schemas, collection }) {
     setSelectAll(!selectAll);
   };
 
+  const { mutate: downloadMutate, isPending } = useMutation({
+    mutationKey: ["download-excel-schema"],
+    mutationFn: async data => {
+      const res = await axios.post(
+        `/api/projects/${projectId}/download`,
+        {
+          collection: data.collection,
+          columns: data.columns,
+        },
+        {
+          responseType: "blob",
+          withCredentials: true,
+        },
+      );
+      return res.data;
+    },
+    onSuccess: data => {
+      const url = window.URL.createObjectURL(new Blob([data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", `${collection}.xlsx`);
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+    },
+    onError: error => {
+      console.error("Failed to download file:", error);
+    },
+  });
+
+  console.log(isPending);
+
   const handleSubmit = async e => {
     e.preventDefault();
     if (!selectedRows.length) {
       return;
     }
-    try {
-      const res = await axios.post(
-        `/api/projects/${projectId}/download`,
-        {
-          collection,
-          columns: selectedRows,
-        },
-        { responseType: "blob", withCredentials: true },
-      );
-      const downloadUrl = window.URL.createObjectURL(new Blob([res.data]));
-      const link = document.createElement("a");
-      link.href = downloadUrl;
-      link.setAttribute("download", `${collection}.xlsx`);
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      window.URL.revokeObjectURL(downloadUrl);
-    } catch (error) {
-      console.error("Failed to download file:", error);
-    }
+    downloadMutate({
+      collection,
+      columns: selectedRows,
+    });
   };
 
   return (
     <div className="flex flex-col w-full h-full space-y-2">
       <div className="flex items-center">
         <h3 className="text-text-800 font-bold flex-grow">Data Schema</h3>
-        <button
-          type="button"
-          onClick={handleSubmit}
-          aria-label="Download Excel"
-        >
-          <RiFileExcel2Line
-            className="flex-shrink-0 text-primary-500"
-            size="25"
-          />
-        </button>
+        {isPending ? (
+          <LoadingLine px={25} />
+        ) : (
+          <button
+            type="button"
+            onClick={handleSubmit}
+            aria-label="Download Excel"
+          >
+            <RiFileExcel2Line
+              className="flex-shrink-0 text-primary-500"
+              size="25"
+            />
+          </button>
+        )}
       </div>
       <table className="w-full h-full text-sm text-center text-text-950 my-2 border border-collapse">
         <thead className="text-sm uppercase border border-collapse">
